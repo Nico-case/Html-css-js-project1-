02f93604e911d17b10debf09f384931831c9fa6c
Nico-case <97565483+Nico-case@users.noreply.github.com>
2022-02-12 12:20:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.static("public"))
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT,() =>{
+console.log(`servidor iniciado en el puerto ${PORT}`);
+})
